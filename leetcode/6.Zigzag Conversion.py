5db3bb4af2e1269cc658a2e10ed7404c3c6e5223
Han-Xiangming <2728513634@qq.com>
2023-01-16 22:20:17 +0800

    Upload some files

---
+++ leetcode/6.Zigzag Conversion.py
@@ -0,0 +1,22 @@
+"""
+【问题描述】
+将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行Z 字形排列。
+比如输入字符串为 "PAYPALISHIRING"行数为3时，排列如下：
+P   A   H   N
+A P L S I I G
+Y   I   R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+请你实现这个将字符串进行指定行数变换的函数：[string convert(string s, int numRows)];
+"""
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2: return s
+        res = ["" for _ in range(numRows)]
+        i, flag = 0, -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1: flag = -flag
+            i += flag
+        return "".join(res)
