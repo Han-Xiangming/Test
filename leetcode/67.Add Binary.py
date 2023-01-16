18afbd49886705f8d90bb7f5dc0bb149f85406f7
Han-Xiangming <2728513634@qq.com>
2023-01-16 21:36:51 +0800

    Upload some files

---
+++ leetcode/67.Add Binary.py
@@ -0,0 +1,12 @@
+"""
+【题目描述】
+给你两个二进制字符串a和b，以二进制字符串的形式返回它们的和。
+【示例】
+输入: a = "11", b = "1"
+输出: "100"
+"""
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a, 2) + int(b, 2))[2:]
