24db8a77f5f1db7fc4eca3add45ec0ef83963016
Han-Xiangming <2728513634@qq.com>
2023-01-16 21:49:56 +0800

    Upload some files

--- leetcode/3.Longest Substring Without Repeating Characters.py
+++ leetcode/3.Longest Substring Without Repeating Characters.py
@@ -7,10 +7,16 @@
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 """
 
-s = input()
-sl, c = [], []
-sl.extend(iter(s))
-for i in range(len(sl)):
-    if sl[i] == sl[i + 1]:
-        c.clear()
-    c.append(sl[i])
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        hash_set = set()
+        left = 0
+        for right, ch in enumerate(s):
+            while ch in hash_set:
+                hash_set.remove(s[left])
+                left += 1
+            ans = max(ans, right - left + 1)
+            hash_set.add(ch)
+        return ans
